8b60018c02563c2e3459ae17236cc721430a68b9
Bablash <72317250+Bablash@users.noreply.github.com>
2020-12-10 03:17:48 +0700

    Add files via upload

--- Polinomial.cpp
+++ Polinomial.cpp
@@ -1,70 +1,94 @@
 #include <iostream>
 #include "Polinomial.h"
+#include <string>
+#include <stdio.h>
 
 using namespace std;
-int Polinomial::count = 0;
 
-bool test1(int x) { //test for the degree = 0
-	Polinomial test(0);
-	test.setX(x);
-	int expected = *test.getCoeff(); //since the degree of the polynomial is 0, it will only have a coefficient multiplied by x^0 and it's equal to 1 -> we get a coefficient*1
-	cout << endl << endl << "Test 1";
-	cout << endl << test.toString();
-	cout << endl << "The expected result for degree n = 0 is ----------> " << expected;
-	cout << endl << "The result of the programm for degree n = 0 is ---> " << test.calc();
-	if (test.getValue() == expected) 
-		return true;
-	else 
-		return false;
+void Polinomial::setN(int n1) {
+	n = n1;
 }
 
-bool test2(int x) { //test for the degree = - 1
-	Polinomial test(-1);
-	int expected = 0;
-	cout << endl << endl << "Test 2";
-	cout << endl << test.toString();
-	cout << endl << "The expected result for degree n = - 1 is --------> " << expected;
-	cout << endl << "The result of the programm for degree n = -1 is ---> " << test.calc();
-	if (test.getValue() == expected) //if the degree of the polynomial is negative, the program will not enter the array of its filling and the polynomial will take the value set initially, i.e. 0
-		return true;
-	else
-		return false;
+void Polinomial::setX(int x1) {
+	x = x1;
 }
 
-bool test3(int x) { ////test for the degree = 4
-	Polinomial test(4);
-	int expected = -95;
-	cout << endl << endl << "Test 3";
-	cout << endl << test.toString();
-	cout << endl << "The expected result for degree n = 4 is ----------> " << expected;
-	cout << endl << "The result of the programm for degree n = 4 is ---> " << test.calc();
-	if (test.getValue() == expected)
-		return true;
-	else
-		return false;
+
+int Polinomial::getValue(){
+	return value;
+}
+int Polinomial::getN() {
+	return n;
 }
 
-int main() {
-	Polinomial first (5);
-	first.setX(3);
-	Polinomial copy(first);
-	Polinomial firstf;
-	cout << copy.toString();
-	cout << endl << "The value of a polynomial of degree 5 of x = 3:  " << first.calc();
+int Polinomial::getX() {
+	return x;
+}
 
-	if (test1(3))
-		cout << endl << "Test 1 is true";
-	else cout << "Test 1 is false";
+int* Polinomial::getCoeff() {
+	return coeff;
+}
 
-	if (test2(3))
-		cout << endl << "Test 2 is true";
-	else cout << "Test 2 is false";
 
-	if (test3(3))
-		cout << endl << "Test 3 is true";
-	else cout << endl << "Test 3 is false";
+int Polinomial::getCount() {
+	return count;
+}
 
-	cout << endl << endl << "the count of objects:  " << first.getCount();
+int Polinomial::pow(int x, int n) {
+	int poww = 1;
+	for (int i = n; i > 0; i--)
+		poww *= x;
+	return poww;
+}
 
-	return 0;
+int Polinomial::calc() {
+	int value1 = 0;
+	for (int i = 0, k = n; i <= n && k >= 0; k--, i++) {
+		value1 += coeff[i] * pow(x, k);
+	}
+	value = value1;
+	return value1;
+}
+
+char* Polinomial::toString() {
+	char* str = new char[255]{""};
+	char* buff = new char[20];
+	for (int i = 0; i <= n; i++)
+	{
+		if (coeff[i] < 0 or i == 0)
+			sprintf_s(buff, 20, "%d*x^%d", coeff[i], n - i);
+		else
+			sprintf_s(buff, 20, "+%d*x^%d", coeff[i], n - i);
+		strcat_s(str, 255, buff);
+	}
+	char* ch = str;
+	return ch;
+}
+
+Polinomial::Polinomial(int n1) {
+	n = n1;
+	coeff = new int[n1 + 1];
+	for (int i = 0; i <= n1; i++) {
+		coeff[i] = rand() % 201-100;
+	}
+	count++;
+}
+
+Polinomial::Polinomial() {
+	coeff = NULL;
+}
+
+Polinomial::Polinomial(const Polinomial& other) {
+	n = other.n;
+	x = other.x;
+	coeff = new int[other.n + 1];
+	for (int i = 0; i <= other.n; i++) {
+		coeff[i] = other.coeff[i];
+	}
+	count++;
+}
+
+Polinomial::~Polinomial() {
+	delete[] coeff;
+	count--;
 }
